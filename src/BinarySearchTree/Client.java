c8280648b4064ae5f96165b112058301edbbe0c9
mirrormingzZ <mirrorming@qq.com>
2019-01-03 10:53:03 +0800

    二分搜索树

---
+++ src/BinarySearchTree/Client.java
@@ -0,0 +1,42 @@
+package BinarySearchTree;
+
+/**
+ * @program: DataStructureAndAlgorithms
+ * @description: 测试
+ * @author: mirrorming
+ * @create: 2019-01-02 19:48
+ **/
+
+public class Client {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] arr = {5, 3, 6, 8, 4, 2};
+        for (int num : arr)
+            bst.add(num);
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+//        bst.preOrder();
+//        System.out.println();
+//        bst.inOrder();
+//        System.out.println();
+//        bst.postOrder();
+//        System.out.println();
+
+        bst.levelOrder();
+        System.out.println();
+
+        Integer integer = bst.removeMax();
+        System.out.println(integer);
+
+        bst.preOrderNR();
+        System.out.println();
+
+        System.out.println(bst);
+    }
+}
